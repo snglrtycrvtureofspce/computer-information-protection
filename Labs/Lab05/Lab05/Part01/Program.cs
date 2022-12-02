99c77e4a3278dd71aa400ff51c529273c755b76b
Aleksandr <asp1rantzenevich@gmail.com>
2022-12-02 10:36:28 +0300

    #

---
+++ Labs/Lab05/Lab05/Part01/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.CodeDom;
+using System.IO;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Part01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader read = new StreamReader("C:\\Users\\snglrtycrvtureofspce\\Desktop\\Protection-of-computer-information-main\\ЛР05\\pass\\Part01\\Code.txt");
+            string text = read.ReadToEnd();
+            read.Close();
+            text = text.ToUpper();
+            Console.WriteLine($"Ваш введенный текст в текстовый файл\n{text}");
+            StreamReader readCode = new StreamReader("C:\\Users\\snglrtycrvtureofspce\\Desktop\\Protection-of-computer-information-main\\ЛР05\\pass\\Part01\\Pass.txt");
+            string code = readCode.ReadToEnd();
+            readCode.Close();
+            code = code.ToUpper();
+            Console.WriteLine($"Ваш введенный шифр в текстовый файл\n{code}");
+            string[] binary = new string[text.Length];//двоичный код
+            binary = codeBi(text);
+            int[] de = new int[text.Length];//десятичный код
+            de = codeDe(binary);
+            string[] binary2 = new string[code.Length];//двоичный код для шифра 
+            binary2 = codeBi(code);
+            int[] de2 = new int[code.Length];//десятичный код для шифра 
+            de2 = codeDe(binary2);
+            Console.WriteLine($"Шифр для слова {text}");
+            PrintS(binary);
+            PrintI(de);
+            Console.WriteLine($"Шифр для слова {code}");
+            PrintS(binary2);
+            PrintI(de2);
+            int[] last = new int[de.Length];
+            last = xor(de, de2);
+            Console.Write("Результат: ");
+            PrintI(last);
+            Console.ReadKey();
+        }
+        static public string[] codeBi(string text)
+        {
+            int index = 0;
+            string[,] code = {  { "A", "B", "C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"},
+                                { "01000001","01000010","01000011","01000100","01000101","01000110","01000111","01001000","01001001","01001010","01001011","01001100","01001101","01001110","01001111","01010000","01010001","01010010","01010011","01010100","01010101","01010110","01010111","01011000","01011001","01011010"} };
+            string[] binary = new string[text.Length];
+            int[] de = new int[text.Length];
+            for (int i = 0; i < text.Length; i++)
+            {
+                for (int j = 0; j < code.Length; j++)
+                {
+                    if (Convert.ToString(text[i]) == code[0, j])
+                    {
+                        index = j;
+                        break;
+                    }
+                }
+                binary[i] = code[1, index];
+
+            }
+            return binary;
+        }
+        static int[] codeDe(string[] binary)
+        {
+            int[] de = new int[binary.Length];
+            for (int i = 0; i < binary.Length; i++)
+            {
+                de[i] = Convert.ToInt32(binary[i], 2);
+            }
+            return de;
+        }
+        static void PrintS(string[] array)
+        {
+            for(int i = 0; i <array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.Write("\n");
+        }
+        static void PrintI(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.Write("\n");
+        }
+        static int[] xor(int[] de, int[] de2)
+        {
+            int[] last = new int[de.Length];
+            for(int i = 0; i < de.Length; i++)
+            {
+                last[i] = de[i] ^ de2[i];
+            }
+            return last;
+        }
+    }
+}
---
+++ Labs/Lab05/Lab05/Part02/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part02
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            string original = "test text";
+            using (Aes myAes = Aes.Create())
+            {
+
+                byte[] encrypted = EncryptStringToBytes_Aes(original, myAes.Key, myAes.IV);
+
+                string roundtrip = DecryptStringFromBytes_Aes(encrypted, myAes.Key, myAes.IV);
+
+                Console.WriteLine("Оригинальный текст:   {0}", original);
+                Console.WriteLine("Шифр: ");
+                for (int i = 0; i < encrypted.Length; i++)
+                {
+                    Console.Write(encrypted[i]);
+                }
+                Console.WriteLine("\nРасшифрованный текст:   {0}", roundtrip);
+
+            }
+            Console.ReadKey();
+        }
+        static byte[] EncryptStringToBytes_Aes(string plainText, byte[] Key, byte[] IV)
+        {
+            //проверка не ошибки 
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+            byte[] encrypted;
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plainText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+            return encrypted;
+        }
+
+        static string DecryptStringFromBytes_Aes(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+            string plaintext = null;
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            return plaintext;
+        }
+    }
+}
