8facaa636a652c30f1c0946da7c175449f94493b
snglrtycrvtureofspce <asp1rantzenevich@gmail.com>
2023-04-07 15:23:14 +0300

    #

---
+++ Labs/Lab12-13/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Numerics;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("---------------------------------------");
+            Tasks tasks = new Tasks();
+            tasks.Task1RSA();
+            Console.WriteLine("---------------------------------------");
+            tasks.Task2AlGamal();
+            Console.WriteLine("---------------------------------------");
+        }
+    }
+
+    class Tasks
+    {
+        public void Task2AlGamal()
+        {
+            int p = 23;
+            int g = 5;
+
+            int x = 9;
+            int k = 19;
+            int m = 3;
+
+            int y = (int)BigInteger.ModPow(g, x, p);
+            int r = (int)BigInteger.ModPow(g, k, p);
+            int s = ModInverse(k, p - 1) * (m - x * r) % (p - 1);
+            bool isValid = VerifySignature(p, g, y, m, r, s);
+
+            Console.WriteLine($"Открытый ключ:  y = {y} \nПодпись: (r, s) = ({r},{s}) \nПроверка подписи: {isValid}");
+        }
+
+        public void Task1RSA()
+        {
+            BigInteger n = 33;
+            BigInteger e_ = 3;
+            Tuple<BigInteger, BigInteger>[] messages = new Tuple<BigInteger, BigInteger>[]
+            {
+                Tuple.Create(new BigInteger(17), new BigInteger(8)),
+                Tuple.Create(new BigInteger(10), new BigInteger(14)),
+                Tuple.Create(new BigInteger(24), new BigInteger(18))
+            };
+            foreach (Tuple<BigInteger, BigInteger> message in messages)
+            {
+                BigInteger M = message.Item1;
+
+                BigInteger s = message.Item2;
+
+                BigInteger M_ = BigInteger.ModPow(s, e_, n);
+
+                if (M == M_)
+                {
+                    Console.WriteLine($"Подпись верна M = {M}");
+                }
+                else
+                {
+                    Console.WriteLine($"Подпись верна M = {M}");
+                }
+            }
+        }
+
+        static int ModInverse(int a, int m)
+        {
+            int m0 = m, t, q;
+            int x0 = 0, x1 = 1;
+
+            if (m == 1)
+                return 0;
+
+            while (a > 1)
+            {
+                q = a / m;
+                t = m;
+                m = a % m;
+                a = t;
+                t = x0;
+                x0 = x1 - q * x0;
+                x1 = t;
+            }
+
+            if (x1 < 0)
+                x1 += m0;
+
+            return x1;
+        }
+
+        static bool VerifySignature(int p, int g, int y, int m, int r, int s)
+        {
+            int w = ModInverse(s, p - 1);
+            int u1 = (int)(m * w % (p - 1));
+            int u2 = (int)(r * w % (p - 1));
+            int v = (int)(BigInteger.ModPow(g, u1, p) 
+                * BigInteger.ModPow(y, u2, p) % p % (p - 1));
+            return v != r;
+        }
+    }
+}
---
+++ Labs/Lab12-13/text/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Lab12-13")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab12-13")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("bdd69d22-e837-49f9-8b09-4abb51c83b7d")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
